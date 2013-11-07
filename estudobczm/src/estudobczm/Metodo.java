2a9ae3c47d78fe4f64ac1baadf5592ae2a8dc8ac
lhcabral <luizhenrique.be@gmail.com>
2013-11-07 15:53:43 -0300

    #3 metodo completo

--- estudobczm/src/estudobczm/Metodo.java
+++ estudobczm/src/estudobczm/Metodo.java
@@ -1,3 +1,8 @@
+package estudobczm;
+
+import javax.swing.JOptionPane;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,9 +18,13 @@ public class Metodo {
 	public void recebeParametros(String nome){
 		if(nome != null){
 			//resultado mostrando o nome digitado
+                    JOptionPane.showMessageDialog(null, "Seja bem vindo"+ nome+" ao treinamento o Git Hub");
+                            
 		}
 		else{
 			//resultado informando o erro por falta do nome
+                    JOptionPane.showMessageDialog(null,"Digite seu nome!");
+                
 		}
 	}
 	
