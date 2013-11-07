ef9a42731c37586d8bf35301b32e6fdeafeb57e5
lhcabral <luizhenrique.be@gmail.com>
2013-11-07 15:34:19 -0300

    #3 meu netbeans ta bugado

---
+++ estudobczm/metodo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author luiz
+ */
+public class Metodo {
+    
+	public void recebeParametros(String nome){
+		if(nome != null){
+			//resultado mostrando o nome digitado
+		}
+		else{
+			//resultado informando o erro por falta do nome
+		}
+	}
+	
+}
